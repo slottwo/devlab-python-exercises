c464da455abeb0a90dfb87cdbfe7f0540cafe90d
slottwo <khaled.phg+github@gmail.com>
2023-03-14 15:07:17 -0300

    Remove a non-requested exercise

--- bank/client.py
+++
@@ -1,6 +0,0 @@
-class Client:
-    def __init__(self, first_name: str, last_name: str, cpf: str) -> None:
-        self.name = first_name, last_name
-        self.cpf = cpf
-
-        self.id = hash(self)
